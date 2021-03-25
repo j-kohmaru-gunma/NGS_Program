91d79cc9081c7899d57cd92d460e48ef4358692a
j-kohmaru-gunma <80726974+j-kohmaru-gunma@users.noreply.github.com>
2021-03-25 09:40:10 +0900

    Add files via upload

---
+++ 210325/test_script.sh
@@ -0,0 +1,7 @@
+cd testdata
+
+perl TSS_filter TSS.sort.bed test.CpG.bedGraph
+
+perl Coverage_filter.pl -i input.txt -c input_cov.txt -min 5 -max 1000 -o All_methyl_cov5-1000.bdg
+
+perl TSS_count.pl All_methyl_cov5-1000.bdg 
