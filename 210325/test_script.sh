f3c8dd7a0fd057eed55329924035375e9c483792
j-kohmaru-gunma <80726974+j-kohmaru-gunma@users.noreply.github.com>
2021-04-01 12:48:26 +0900

    Delete 210325 directory

--- 210325/test_script.sh
+++
@@ -1,7 +0,0 @@
-cd testdata
-
-perl ../TSS_filter TSS.sort.bed test.CpG.bedGraph
-
-perl ../Coverage_filter.pl -i input.txt -c input_cov.txt -min 5 -max 1000 -o All_methyl_cov5-1000.bdg
-
-perl ../TSS_count.pl output/All_methyl_cov5-1000.bdg 
